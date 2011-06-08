04bf7b0a285c907c99c54a78fd63dde73c1484ae
Peter Mallett <peter.mallett@gmail.com>
2011-06-08 19:52:09 -0400

    Add problem 3.

---
+++ 3.py
@@ -0,0 +1,35 @@
+from math import sqrt
+
+#find the largest prime factor of 600851475143
+number = 600851475143
+
+def is_prime(n):
+	if (n < 2):
+		return False
+
+	for x in range(2, int(sqrt(n))+1):
+		if (n % x == 0):
+			return False
+
+	return True
+
+def find_next_prime(n):
+	x = n
+	while(True):
+		x += 1
+		if (is_prime(x)):
+			return x
+
+half = int(number/2)
+
+prime = 2
+factors = []
+answer = 0
+while (answer == 0):
+	if (number % prime == 0):
+		factors.append(prime)
+	prime = find_next_prime(prime)
+
+	if (len(factors) == 4):
+		print(factors)
+		exit()
