44adf68c0e892b4dcb347874aefa39292e4010a8
Peter Mallett <peter.mallett@gmail.com>
2011-06-15 19:21:40 -0400

    Add problem 7.

---
+++ 007.py
@@ -0,0 +1,28 @@
+﻿# What is the 10001st prime number?
+
+from math import sqrt
+
+def is_prime(n):
+	if (n < 2):
+		return False
+
+	for x in range(2, int(sqrt(n))+1):
+		if (n % x == 0):
+			return False
+
+	return True
+	
+def find_next_prime(n):
+	x = n
+	while(True):
+		x += 1
+		if (is_prime(x)):
+			return x
+			
+number = 1
+prime = 2
+while (number != 10001):
+	prime = find_next_prime(prime)
+	number += 1
+
+print(prime)
